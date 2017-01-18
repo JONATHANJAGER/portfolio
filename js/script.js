5c70c0bda73978359ed91cf5491d3f21d2e43b46
jonathanjager <jagerbill11@gmail.com>
2017-01-18 17:58:06 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// Bio page open and close overlay //
+function openPic() {
+	document.getElementById("clickpic").style.width="100%";
+}
+
+function closePic() {
+	document.getElementById("clickpic").style.width="0%";
+}
